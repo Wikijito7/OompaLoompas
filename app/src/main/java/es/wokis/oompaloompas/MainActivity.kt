91a8e40de59515c35d06ab0303984f03c5217fb9
Wikijito7 <wikyfg@gmail.com>
2022-09-20 19:09:14 +0200

    First commit

---
+++ app/src/main/java/es/wokis/oompaloompas/MainActivity.kt
@@ -0,0 +1,11 @@
+package es.wokis.oompaloompas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/es/wokis/oompaloompas/MainApplication.kt
@@ -0,0 +1,7 @@
+package es.wokis.oompaloompas
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application()
